1899157c4473c0eb8d4893e08af346bbaa4c0cfd
Anton <a.chertok@akatech.com.ua>
2023-08-02 19:49:21 +0300

    Update WaybillHistory.tsx

--- src/components/WaybillHistory/WaybillHistory.tsx
+++ src/components/WaybillHistory/WaybillHistory.tsx
@@ -4,6 +4,7 @@ const WaybillHistory = () => {
   return (
     <aside style={{ backgroundColor: "#bc87d6" }}>
       <h2>Історія</h2>
+      <button type="button">Очистити історію</button>
       <WaybillList />
     </aside>
   );
