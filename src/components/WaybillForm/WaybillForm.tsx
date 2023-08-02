6703cb4dcd2934be23e87d00178a51ee5db86372
Anton <a.chertok@akatech.com.ua>
2023-08-03 00:54:05 +0300

    Update WaybillForm.tsx

--- src/components/WaybillForm/WaybillForm.tsx
+++ src/components/WaybillForm/WaybillForm.tsx
@@ -1,17 +1,40 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
 const waybillValidationPattern = "^[1-9][0-9]{13}$";
 
+interface IFormElementsWaybill extends HTMLFormControlsCollection {
+  waybillNumber: HTMLInputElement;
+}
+
 const WaybillForm = () => {
+  const [waybillNumber, setWaybillNumber] = useState<string>("");
+
+  const onWaybillChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setWaybillNumber(e.target.value);
+  };
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const elements = form.elements as IFormElementsWaybill;
+    const value = elements.waybillNumber.value;
+
+    console.log(value);
+  };
+
   return (
-    <form style={{ backgroundColor: "#cc4e84" }}>
+    <form style={{ backgroundColor: "#cc4e84" }} onSubmit={onSubmit}>
       <label>
         {/* Введіть ТТН */}
         <input
           type="text"
           name="waybillNumber"
           required
-          pattern={waybillValidationPattern}
+          // pattern={waybillValidationPattern}
           title="Waybill number may contain 10 digits"
           placeholder="ТТН"
+          value={waybillNumber}
+          onChange={onWaybillChange}
         />
       </label>
       <button type="submit">Отримати статус ТТН</button>
