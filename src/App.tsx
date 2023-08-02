3bb61bdc228315331a8e99f3954166f88c337d74
Anton <a.chertok@akatech.com.ua>
2023-08-02 10:33:10 +0300

    env update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  console.log(process.env.REACT_APP_NP_API_ENTRY_POINT);
+  console.log(process.env.REACT_APP_NP_API_KEY);
   return (
     <div className="App">
       <header className="App-header">
