254068b19244e2694cd48cd789983e7fa64f98eb
Anton <a.chertok@akatech.com.ua>
2023-08-02 19:45:01 +0300

    waybill info markup

--- src/components/WaybillForm/WaybillForm.tsx
+++ src/components/WaybillForm/WaybillForm.tsx
@@ -1,5 +1,22 @@
+const waybillValidationPattern = "^[1-9][0-9]{13}$";
+
 const WaybillForm = () => {
-  return <form style={{ backgroundColor: "#cc4e84" }}>Waybill Form</form>;
+  return (
+    <form style={{ backgroundColor: "#cc4e84" }}>
+      <label>
+        {/* Введіть ТТН */}
+        <input
+          type="text"
+          name="waybillNumber"
+          required
+          pattern={waybillValidationPattern}
+          title="Waybill number may contain 10 digits"
+          placeholder="ТТН"
+        />
+      </label>
+      <button type="submit">Отримати статус ТТН</button>
+    </form>
+  );
 };
 
 export default WaybillForm;
--- src/components/WaybillInfo/WaybillInfo.tsx
+++ src/components/WaybillInfo/WaybillInfo.tsx
@@ -1,5 +1,11 @@
 const WaybillInfo = () => {
-  return <div style={{ backgroundColor: "#e69443" }}>Waybill Info</div>;
+  return (
+    <div style={{ backgroundColor: "#e69443" }}>
+      <p>Статус доставки: </p>
+      <p>Відправлено: Відділення 17</p>
+      <p>Отримано: Відділення 13</p>
+    </div>
+  );
 };
 
 export default WaybillInfo;
