6ed36fbe8defcbd3a30001fae64762a78dd81d4d
Anton <a.chertok@akatech.com.ua>
2023-08-03 19:16:17 +0300

    debounce

--- src/components/UIKit/Select/Select.tsx
+++ src/components/UIKit/Select/Select.tsx
@@ -1,14 +1,22 @@
-import { FC } from "react";
+import { ChangeEvent, FC } from "react";
 import { ISelectItem } from "types/types";
+const debounce = require("lodash.debounce");
 
 interface ISelectProps {
   list: ISelectItem[];
+  value: string;
+  onSelectChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Select: FC<ISelectProps> = ({ list }) => {
+const Select: FC<ISelectProps> = ({ list, value, onSelectChange }) => {
   return (
     <label>
-      <input type="text" name="city" />
+      <input
+        type="text"
+        name="city"
+        value={value}
+        onChange={debounce(onSelectChange, 200)}
+      />
       <ul style={{ backgroundColor: "#7188a8" }}>
         {list.map(({ id, title }) => (
           <li key={id}>
--- src/components/Warehouse/Warehouse.tsx
+++ src/components/Warehouse/Warehouse.tsx
@@ -1,20 +1,32 @@
 import Select from "components/UIKit/Select";
+import { ChangeEvent, useState } from "react";
 import { useGetSettlementsQuery } from "redux/warehouse/warehouseApi";
 import { settlementsListTransform } from "utils/settlementsListTransform";
 
 const Warehouse = () => {
-  const { data: response } = useGetSettlementsQuery("ки");
-  console.log(response);
+  const [selectValue, setSelectValue] = useState<string>("");
+
+  const { data: response } = useGetSettlementsQuery(selectValue);
+
+  const onSelectChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setSelectValue(e.target.value);
+  };
 
   if (!response) return null;
 
   const settlements = settlementsListTransform(response.data);
 
+  // console.log(response);
   return (
     <div>
       <h1>Список відділень</h1>
       <div style={{ display: "flex" }}>
-        <Select list={settlements} />
+        <Select
+          list={settlements}
+          value={selectValue}
+          onSelectChange={onSelectChange}
+        />
         <div style={{ backgroundColor: "#1fa3a1" }}>
           <ul>
             <li>Warehouse 1</li>
