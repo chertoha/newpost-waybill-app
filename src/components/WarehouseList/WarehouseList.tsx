1110be35a5b9d398ab8cc5e65a85d8166213e8cf
Anton <a.chertok@akatech.com.ua>
2023-08-04 14:29:02 +0300

    Update WarehouseList.tsx

--- src/components/WarehouseList/WarehouseList.tsx
+++ src/components/WarehouseList/WarehouseList.tsx
@@ -3,18 +3,18 @@ import { useGetWarehousesQuery } from "redux/warehouse/warehouseApi";
 import { StorageService } from "services/StorageService";
 import { IWarehouse } from "types/types";
 
-const storage = new StorageService<{ page: number; list: IWarehouse[] }>(
-  "warehouseList"
-);
+// const storage = new StorageService<{ page: number; list: IWarehouse[] }>(
+//   "warehouseList"
+// );
 
 interface IWarehouseListProps {
   cityRef: string | null;
 }
 
 const WarehouseList: FC<IWarehouseListProps> = ({ cityRef }) => {
-  const [list, setList] = useState<IWarehouse[]>(
-    () => storage.get()?.list || []
-  );
+  //   const [list, setList] = useState<IWarehouse[]>(
+  //     () => storage.get()?.list || []
+  //   );
   const [page, setPage] = useState<number>(1);
 
   const increasePage = () => {
@@ -28,19 +28,21 @@ const WarehouseList: FC<IWarehouseListProps> = ({ cityRef }) => {
     }
   );
 
-  useEffect(() => {
-    const currentPage = storage.get()?.page;
-    if (response && currentPage !== page) {
-      console.log("page=", page);
-      setList((prevList) => [...prevList, ...response.data]);
-      storage.set({ page, list: [...list, ...response.data] });
-    }
-  }, [list, page, response]);
+  if (!response) return null;
+
+  //   useEffect(() => {
+  //     const currentPage = storage.get()?.page;
+  //     if (response && currentPage !== page) {
+  //       console.log("page=", page);
+  //       setList((prevList) => [...prevList, ...response.data]);
+  //       storage.set({ page, list: [...list, ...response.data] });
+  //     }
+  //   }, [list, page, response]);
 
   return (
     <div style={{ backgroundColor: "#1fa3a1" }}>
       <ul>
-        {list.map(({ Ref: ref, Description: description }) => (
+        {response.data.map(({ Ref: ref, Description: description }) => (
           <li key={ref}>{description}</li>
         ))}
       </ul>
