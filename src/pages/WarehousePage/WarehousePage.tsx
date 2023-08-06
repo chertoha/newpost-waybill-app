5f1c400c3a1ed92bfaae433019c488cdb6596d38
Anton <a.chertok@akatech.com.ua>
2023-08-06 20:21:10 +0300

    helmet - header, meta, titles

--- src/pages/WarehousePage/WarehousePage.tsx
+++ src/pages/WarehousePage/WarehousePage.tsx
@@ -1,7 +1,19 @@
 import Warehouse from "components/Warehouse/Warehouse";
+import { Helmet } from "react-helmet";
 
 const WarehousePage = () => {
-  return <Warehouse />;
+  return (
+    <>
+      <Helmet>
+        <title>Список відділень</title>
+        <meta
+          name="description"
+          content="Пошук відділень Нової Пошти за назвою населеного пункту"
+        />
+      </Helmet>
+      <Warehouse />
+    </>
+  );
 };
 
 export default WarehousePage;
--- src/pages/WaybillPage/WaybillPage.tsx
+++ src/pages/WaybillPage/WaybillPage.tsx
@@ -1,7 +1,16 @@
 import Waybill from "components/Waybill";
+import { Helmet } from "react-helmet";
 
 const WaybillPage = () => {
-  return <Waybill />;
+  return (
+    <>
+      <Helmet>
+        <title>Статус ТТН</title>
+        <meta name="description" content="Статус ТТН Нової Пошти за номером" />
+      </Helmet>
+      <Waybill />;
+    </>
+  );
 };
 
 export default WaybillPage;
