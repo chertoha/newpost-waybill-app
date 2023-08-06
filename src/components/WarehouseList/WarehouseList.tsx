dda01a9b3289fd74a574860ffcc548deb9a2756e
Anton <a.chertok@akatech.com.ua>
2023-08-06 13:15:05 +0300

    warehouse list update

--- src/components/WarehouseList/WarehouseList.tsx
+++ src/components/WarehouseList/WarehouseList.tsx
@@ -1,5 +1,5 @@
 import SubTitle from "components/UIKit/SubTitle";
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { useLazyGetWarehousesQuery } from "redux/warehouse/warehouseApi";
 import { StorageService } from "services/StorageService";
 import { IWarehouse } from "types/types";
@@ -14,52 +14,46 @@ interface IWarehouseListProps {
 }
 
 const WarehouseList: FC<IWarehouseListProps> = ({ cityRef }) => {
+  // const [currentCityRef, setCurrentCityRef] = useState<string | null>(
+  //   () => cityRef
+  // );
   const [page, setPage] = useState<number>(1);
   const [list, setList] = useState<IWarehouse[]>(
     () => storage.get()?.list || []
   );
 
-  // console.log(list);
+  // useEffect(() => {
+  //   if (cityRef !== currentCityRef) {
+  //     setPage(1);
+  //     setList([]);
+  //     storage.remove();
+  //     setCurrentCityRef(cityRef);
+  //   }
+  // }, [cityRef, currentCityRef]);
 
   const increasePage = () => {
     setPage((prevState) => prevState + 1);
   };
-
-  // const { data: response } = useGetWarehousesQuery(
-  //   { cityRef, page: page.toString() },
-  // {
-  //   skip: cityRef ? false : true,
-  // }
-  // );
 
   const [fetchWarehouses] = useLazyGetWarehousesQuery();
 
   useEffect(() => {
     if (!cityRef) return;
     if (page === storage.get()?.page) return;
+    // if (cityRef === currentCityRef) return;
 
-    fetchWarehouses({ cityRef, page: page.toString() }).then(
-      ({ data: response }) => {
-        const warehouses = response?.data;
-        if (warehouses) {
-          setList((prevList) => [...prevList, ...warehouses]);
-          storage.set({ page, list: [...list, ...warehouses] });
-          console.log(response);
-        }
+    fetchWarehouses({
+      cityRef,
+      page: page.toString(),
+    }).then(({ data: response }) => {
+      const warehouses = response?.data;
+      if (warehouses) {
+        setList((prevList) => [...prevList, ...warehouses]);
+        storage.set({ page, list: [...list, ...warehouses] });
+        console.log(response);
       }
-    );
+    });
   }, [fetchWarehouses, cityRef, page, list]);
-
-  // useEffect(() => {
-  //   const currentPage = storage.get()?.page;
-  //   if (response && currentPage !== page) {
-  //     console.log("page=", page);
-  // setList((prevList) => [...prevList, ...response.data]);
-  // storage.set({ page, list: [...list, ...response.data] });
-  //   }
-  // }, [list, page, response]);
-
-  // if (!response) return null;
 
   return (
     <ListWrapper>
